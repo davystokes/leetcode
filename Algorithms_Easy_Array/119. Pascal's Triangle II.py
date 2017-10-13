d3d8046d201f2c5eabff1b495f08fd46102c8072
davystokes <32062013+davystokes@users.noreply.github.com>
2017-10-13 21:50:35 +0800

    the same as 118

---
+++ Algorithms_Easy_Array/119. Pascal's Triangle II.py
@@ -0,0 +1,23 @@
+'''
+Given an index k, return the kth row of the Pascal's triangle.
+
+For example, given k = 3,
+Return [1,3,3,1].
+
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
+'''
+
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        row = [1]
+        for _ in range(rowIndex):
+            row = [x + y for x, y in zip([0]+row, row+[0])]
+        return row
+
+'''
+和上一道题几乎一摸一样的解法 注意一下用zip（）函数代替map函数可以提高速度
